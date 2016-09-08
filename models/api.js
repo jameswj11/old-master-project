845ae1b1748a9e4f4aafe437f059fd499aecf37a
James Johnson <jameswj111@gmail.com>
2016-09-07 20:14:22 -0400

    add ability to query artists on api and front-end

--- models/api.js
+++ models/api.js
@@ -4,8 +4,16 @@ const request  = require('request')
 const jsonData = require('../old-masters.json')
 
 module.exports = {
-  getArt: function(req, res, next){
-    res.art = jsonData;
+  searchArt: function(req, res, next){
+    console.log(req.query)
+    if('artist' in req.query){
+      function filterArtist(artist){
+        return artist.name.toLowerCase().includes(req.query.artist);
+      }
+      res.art = jsonData.artists.filter(filterArtist)
+    } else {
+      res.art = jsonData;
+    }
     next()
   }
 }
